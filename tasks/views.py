707993418674d53cf8b49fcb2c3f88487e3218e2
Agriana <agriana99@gmail.com>
2026-01-04 17:04:01 +0700

    Added Task Create and simple form HTML

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,22 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'description', 'status', 'priority', 'due_date']
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('tasks:task-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user # Add user info
+        return super().form_valid(form) # Save and Redirect
+    
+
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
-    template_name = 'task.html'
+    template_name = 'tasks.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
