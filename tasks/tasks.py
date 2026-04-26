ae423a1e33b86cae3ac1163ed2f3b4696419e33d
Agriana <agriana99@gmail.com>
2026-04-26 15:02:45 +0700

    feat: add notification for every members in organization with Celery

---
+++ tasks/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from django.core.mail import send_mail
+
+from organizations.models import MembershipProfile
+from .models import Task
+
+
+@shared_task
+def notify_org_member_new_task(task_id):
+    task = Task.objects.get(id=task_id)
+    member_emails = MembershipProfile.objects.filter(
+        organization=task.organization
+    ).values_list('user__email', flat=True)
+    for email in member_emails:
+        send_mail(
+            'New Task Created',
+            f'A new task "{task.title}" has been created. Please have a look!',
+            'noreply@taskflow.com',
+            [mail]
+        )
